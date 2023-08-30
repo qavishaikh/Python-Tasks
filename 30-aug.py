65361fc8233850b0451b00bdcc3ef965d99a888b
Qavi <qavi.shaikh11@gmail.com>
2023-08-30 23:37:32 +0500

    For Loop 2

--- 30-aug.py
+++ 30-aug.py
@@ -1,5 +1,13 @@
 print("30-Auguest")
 print("Second Class")
 
-for x in range(5):
-    print(x)
+# for x in range(5):
+#     print(x)
+
+for y in range(2,8,2):
+    print(y)
+
+print("Next Block of code")
+
+for z in range(1,8,2):
+    print(z)
