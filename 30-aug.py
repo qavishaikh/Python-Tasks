111347f534916475e3b584cbb2bfee24a28942b4
Qavi <qavi.shaikh11@gmail.com>
2023-08-31 13:43:52 +0500

    Forr Loop in List 3

--- 30-aug.py
+++ 30-aug.py
@@ -9,5 +9,6 @@ for y in range(2,8,2):
 
 print("Next Block of code")
 
-for z in range(1,8,2):
+for z in range(1,8,2): 
     print(z)
+
---
+++ list.py
@@ -0,0 +1,13 @@
+q = ["qavi","is","a","good","boy"]
+print(q)
+
+for a in q:
+    print(a)
+
+print("Second Statement")
+
+numbers = [1,4,5,10,20]
+sum = 0
+for num in numbers:
+    sum += num
+print("Sum",sum)
