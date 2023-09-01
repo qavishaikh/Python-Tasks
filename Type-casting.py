f60b8299f9e7dd585d1b2e614b3b26935cdf8414
Qavi <qavi.shaikh11@gmail.com>
2023-09-01 10:26:46 +0500

    Type-Casting

---
+++ Type-casting.py
@@ -0,0 +1,22 @@
+print("1-sep")
+
+print("Type Casting")
+
+print("Casting in Integer")
+x = int(1)   
+y = int(2.8) 
+z = int("3") 
+print(x,y,z)
+
+print("Casting in Float")
+a = float(1)    
+b = float(2.8)  
+c = float("3")  
+d = float("4.2")
+print(a,b,c,d)
+
+print("Casting in String")
+e = str("s1") 
+f = str(2)   
+g = str(3.0)  
+print(e,f,g)
